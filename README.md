3df9d9ba9fd9ad17e2bd1465954c9417d0134683
Vedanga17 <vedanga.gupta@gmail.com>
2026-07-04 03:36:55 +0530

    added week4 assignment

--- README.md
+++ README.md
@@ -5,3 +5,4 @@ This repository contains all of my weekly assignments for my DevOps course.
 * **[Week 1: Intro Website (Cricket 101)](./week1_cricket_website)**
 * **[Week 2: React Dice Game](./week2_dice_game)**
 * **[Week 3: E-Commerce UI Page](./week3_ecommerce/)**
+* **[Week 4: Intro to Backend](./week4_backend/)**
--- week4_backend/src/db/connector.js
+++ week4_backend/src/db/connector.js
@@ -1,14 +1,13 @@
 import mongoose from 'mongoose';
 
-
-const DB_connector = async () => {
-    try {
-        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}/${process.env.DB_NAME}`);
-        console.log("MongoDB connected successfully");
+const DB_connector = async () => { // need to write an async function as db connection takes time
+    try { // wrapping in try-catch to handle errors better
+        const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}`); // storing in object
+        console.log("MongoDB connected successfully"); // print success message
     } catch (error) {
-        console.error("MONGODB Connection failed", error);
-        process.exit(1);
+        console.error("MongoDB Connection failed", error); // if error, print error message and also what error has occured.
+        process.exit(1); // exit the process
     }
 }
 
-export default DB_connector;
+export {DB_connector}; // export for further use in index.js
